81dbcab798d18942ad201a8355a0df67dd402fd3
wutsi <herve.tchepannou@gmail.com>
2022-10-05 17:17:08 -0400

    add bye processor

--- src/test/kotlin/com/wutsi/platform/rtm/websocket/processor/ByeProcessorTest.kt
+++ src/test/kotlin/com/wutsi/platform/rtm/websocket/processor/ByeProcessorTest.kt
@@ -1,6 +1,5 @@
 package com.wutsi.platform.rtm.websocket.processor
 
-import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
 import com.wutsi.platform.rtm.dto.Message
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketSession
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -34,7 +32,6 @@ internal class ByeProcessorTest {
         val message = Message(type = MessageType.hello, roomId = "1")
         processor.process(message, session)
 
-        val result = argumentCaptor<TextMessage>()
         verify(context).detach(session)
     }
 }
